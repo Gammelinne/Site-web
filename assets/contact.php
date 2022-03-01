9dff6bdcd52c3a3c9997d290911f717a444b92ba
gammelinne <kylian.renault@viacesi.fr>
2022-03-01 15:03:52 +0100

    Ajout formulaire de contact

---
+++ assets/contact.php
@@ -0,0 +1,28 @@
+<?php
+
+    if (isset($_POST["ville"] && $_POST["email"] && $_POST["nom"] && $_POST["add1"]))
+    {
+        $emailTo = "kylian.renault@viacesi.fr";
+        $emailSubject = "Nouvelle demande de contact";
+        $email = $_POST["email"];
+        $message = $_POST["nom"]. $_POST["prenom"]. "demande à vous contacter. \n Il vit à ". $_POST["ville"]. "\n Merci de répondre au mail suivant". $_POST["email"];
+        $errorMessage = "";
+        $emailDetails = "Détails de la demande ci-dessous.\n\n";
+
+        function cleanString($string)
+        {
+            $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+            return str_replace($bad, "", $string);
+        }
+
+        $emailMessage .= "Adresse mail: " . cleanString($email) . "\n";
+        $emailMessage .= "Message:\n" . cleanString($message);
+        $headers =  "From: " . $email . "\r\n" .
+                    "Reply-To: " . $email . "\r\n" .
+                    "X-Mailer: PHP/" . phpversion();
+        @mail($emailTo, $emailSubject, $emailMessage, $headers);
+
+    }
+    header("Location: http://127.0.0.1:5500");
+    
+?>
